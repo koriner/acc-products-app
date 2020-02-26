c3c85424678d6d793b805b35569a7c03119d0ea8
Shane Korin <sk@distriqt.com>
2020-02-26 17:25:13 +1100

    replaced testing-library with enzyme and cleaned out some of the CRA default stuff

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/**
+ * Generic setup method to render an instance of the component,
+ * which can be used by multiple tests.
+ */
+const setup = () => {
+  const component = shallow(<App />);
+  return component;
+}
+
+// App component tests
+describe('App component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup();
+  });
+
+  describe('rendering', () => {
+
+    it ('should render the App component', () => {
+      const comp = wrapper.find(`[data-testid='App']`);
+      expect(comp.length).toBe(1);
+    });
+
+  });
+
 });

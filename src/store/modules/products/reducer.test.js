cd753c6940475ac00c046d9a26bcca3292b8db6d
Shane Korin <sk@distriqt.com>
2020-02-26 18:06:31 +1100

    added a couple of basic tests for the products reducer

---
+++ src/store/modules/products/reducer.test.js
@@ -0,0 +1,29 @@
+// Product module reducer tests
+
+import { INITIAL_STATE } from './reducer';
+import { GET_PRODUCTS } from './actions';
+import reducer from './reducer';
+
+describe('Products reducer', () => {
+  // Test initial reducer state
+  it ('should return initial state with no input', () => {
+    const state = reducer(undefined, {});
+    expect(state).toEqual(INITIAL_STATE);
+  });
+
+  // Test the GET_PRODUCTS action
+  it ('should return new state for GET_PRODUCTS action', () => {
+    const products = [
+      { id: 1, name: 'Product 1' },
+      { id: 2, name: 'Product 2' },
+    ];
+
+    const state = reducer(undefined, {});
+    const newState = reducer(state, {
+      type: GET_PRODUCTS,
+      payload: { products }
+    });
+
+    expect(newState).toEqual({ products });
+  });
+});
